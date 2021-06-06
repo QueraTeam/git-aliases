56df97abd7419e955fee3b9de4cd93acd378813c
Mohammad Jafari <m.jafari9877@gmail.com>
2021-06-06 15:58:48 +0430

    bugfix in grep regexp & tag push to remote

--- scripts/qsubmit.sh
+++ scripts/qsubmit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git config --local alias.qsubmit "! f() { SUBMIT_MSG=\$(head /dev/urandom | tr -dc a-z | head -c15); branches=\$(git for-each-ref --format '%(refname:short)' refs/heads); tags=\$(git tag -l); git push origin --atomic -f -q \$branches \$tags; git tag -a submit -m \"\$SUBMIT_MSG\" -f; git push origin submit -f; }; f"
+git config --local alias.qsubmit "! f() { SUBMIT_MSG=\$(head /dev/urandom | tr -dc a-z | head -c15); branches=\$(git for-each-ref --format '%(refname:short)' refs/heads); tags=\$(git tag -l | grep -Ev '^submit.+\$'); git tag -a submit -m \"\$SUBMIT_MSG\" -f; git push origin --atomic -f -u -q \$branches \$tags; }; f"
